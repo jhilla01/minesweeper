be5885a5ac8b7e22bfeebd84d21c75276dffa24c
jhilla01 <114031625+jhilla01@users.noreply.github.com>
2023-03-29 18:54:21 -0500

    Java backend for minesweeper api

---
+++ src/main/java/com/jachil/minesweeperapi/MinesweeperGameApp.java
@@ -0,0 +1,13 @@
+package com.jachil.minesweeperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperGame {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinesweeperGame.class, args);
+    }
+
+}
---
+++ src/main/java/com/jachil/minesweeperapi/MinesweeperService.java
@@ -0,0 +1,2 @@
+package com.jachil.minesweeperapi;public class MinesweeperService {
+}
